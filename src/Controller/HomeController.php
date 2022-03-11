8c0fe9074fec505d148bd6faebadd9c2b1665529
Pierre <Victsing@outlook.fr>
2022-03-11 20:19:56 +0100

    add shop route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StudyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,20 +22,31 @@ class HomeController extends AbstractController
     {
         return $this->render('home/about.html.twig');
     }
+
     #[Route('/newNomadikDeviceApp', name: 'app_newNomadikDeviceApp')]
     public function newNomadikDeviceApp(): Response
     {
         return $this->render('home/newNomadikDeviceApp.html.twig');
     }
+
     #[Route('/services', name: 'app_services')]
     public function us(): Response
     {
         return $this->render('home/us.html.twig');
     }
+
     #[Route('/contact', name: 'app_contact')]
     public function contact(): Response
     {
         return $this->render('home/contact.html.twig');
+    }
+
+    #[Route('/shop', name: 'app_shop')]
+    public function shop(StudyRepository $studyRepository): Response
+    {
+        return $this->render('home/shop.html.twig',[
+            'studies' => $studyRepository->findAll()
+        ]);
     }
 
 
